e7dbd1f116cde9c77fb2a262f3086360895dc03f
nita-san <ysnitta@gmail.com>
2024-08-18 21:05:48 +0900

    learn basic type

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,11 @@ fun main() {
     val age = 20
 
     println("Hello $name! Your age is $age")
+
+    val a:Int = 1000
+    val b:String = "log message"
+    val c:Double = 3.14
+    val d:Long = 100_000_000_000_000
+    val e:Boolean = false
+    val f:Char = '\n'
 }
