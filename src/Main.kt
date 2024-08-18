4b710eb0ae6c16207d475cc0b9f0704143ff68f1
nita-san <ysnitta@gmail.com>
2024-08-18 21:35:23 +0900

    learn list, set, map

--- src/Main.kt
+++ src/Main.kt
@@ -10,4 +10,21 @@ fun main() {
     val d:Long = 100_000_000_000_000
     val e:Boolean = false
     val f:Char = '\n'
+
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(17, 2)
+    var sum = greenNumbers.count() + redNumbers.count()
+
+    println("sum value is $sum")
+
+    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+    val requested = "smtp"
+    val isSupported = requested in SUPPORTED
+
+    println("Support for $requested: $isSupported")
+
+    val number2word = mapOf(1 to "one", 2 to "two", 3 to "three")
+    val n = 2
+
+    println("$n is spelt as ${number2word[n]}")
 }
